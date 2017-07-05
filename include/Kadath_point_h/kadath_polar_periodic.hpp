c2d637c8ff27c01011d48b0e9860b48741790097
Grandclement Philippe <philippe.grandclement@obspm.fr>
2017-07-05 11:30:40 +0200

    forgotten .hpp

---
+++ include/Kadath_point_h/kadath_polar_periodic.hpp
@@ -0,0 +1,42 @@
+/*
+    Copyright 2017 Philippe Grandclement
+
+    This file is part of Kadath.
+
+    Kadath is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kadath is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kadath.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __KADATH_POLAR_HPP_
+#define __KADATH_POLAR_HPP_
+
+#include "headcpp.hpp"
+#include "array.hpp"
+#include "array_math.cpp"
+#include "base_tensor.hpp"
+#include "base_spectral.hpp"
+#include "matrice.hpp"
+#include "name_tools.hpp"
+#include "ope_eq.hpp"
+#include "param.hpp"
+#include "point.hpp"
+#include "scalar.hpp"
+#include "tensor.hpp"
+#include "vector.hpp"
+#include "term_eq.hpp"
+#include "system_of_eqs.hpp"
+#include "metric_symphi.hpp"
+
+#include "polar_periodic.hpp"
+
+#endif
