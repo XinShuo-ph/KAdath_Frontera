00b9ad53283f1ef2f352c56ddfd295350baf4985
sauliac <Sylvain.Auliac@obspm.fr>
2020-02-12 15:58:13 +0100

    cmake update

--- Cmake/CMakeLocal.cmake
+++
@@ -1,4 +0,0 @@
-set (FFTW_LIBRARIES "/usr/lib/x86_64-linux-gnu/libfftw3.a")
-set (SCALAPACK_LIBRARIES "/usr/lib/x86_64-linux-gnu/libscalapack-openmpi.so")
-#set (PGPLOT_LIBRARIES "/usr/lib/libpgplot.a")
-#set (GSL_LIBRARIES "/usr/lib/x86_64-linux-gnu/libgsl.a /usr/lib/x86_64-linux-gnu/libgslcblas.a")
