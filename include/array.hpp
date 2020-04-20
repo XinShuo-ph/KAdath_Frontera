34306063384fde793364eaf519ec69d40695816f
Sylvain AULIAC <Sylvain.Auliac@obspm.fr>
2020-04-20 23:59:35 +0200

    Solving bugs introduced with the previous commits.

--- include/array.hpp
+++ include/array.hpp
@@ -131,7 +131,7 @@ template <typename T> class Array : public Memory_mapped {
 	     * Defines a new value for \c dimensions and reallocate data with respect to the new total size. Values are
 	     * discarded.
 	     */
-	    void resize(Dim_array const & new_dim) {dimensions = new_dim; nbr = dimensions.total_size(); data.resize(nbr);}
+	    void resize(Dim_array const & new_dim);
 
 	public:
         //! Swaps contents between the two arrays (carefull with arrays of allocated pointers).
@@ -376,6 +376,13 @@ template <typename T> bool Array<T>::is_increasing() const {
         dimensions.save(fd) ;
         fwrite_be(&nbr, sizeof(int), 1, fd) ;
         fwrite_be(data.get_data(), sizeof(T), nbr, fd) ;
+    }
+
+    template<typename T> inline void Array<T>::resize(Dim_array const & new_dim) {
+        dimensions.resize(new_dim.get_size());
+        dimensions = new_dim;
+        nbr = dimensions.total_size();
+        data.resize(nbr);
     }
 
 // Display
--- include/memory.hpp
+++ include/memory.hpp
@@ -244,10 +244,7 @@ namespace Kadath {
     template<typename T,typename S> inline Memory_mapped_array<T,S> &
     Memory_mapped_array<T,S>::operator=(Memory_mapped_array<T,S> const &source)
     {
-        if(size != source.size) {
-            Memory_mapper::release_memory<T>(data,static_cast<std::size_t>(size));
-            size = source.size;
-        }
+        this->resize(source.size);
         for(std::size_t i{0};i<size;i++) data[i] = source.data[i];
         return *this;
     }
