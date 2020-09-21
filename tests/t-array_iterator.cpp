2dfaf4fae630fca763aab75a49a56d4ec4866379
sauliac <Sylvain.Auliac@obspm.fr>
2020-09-21 18:25:16 +0200

    Added the possibility to shift array major indexation after/before Read/write operations from/to files

--- tests/t-array_iterator.cpp
+++ tests/t-array_iterator.cpp
@@ -35,6 +35,25 @@ int main(int argc,char *argv[])
     int const nbr {d[0]*d[1]*d[2]*d[3]*d[4]};
     Array<int> test_array{dimensions};
     for(int i=0;i<nbr;i++) test_array.set_data()[i] = i;
+
+    for(int i=0;i<nbr;i++) {
+        int const ldmi{test_array.to_last_dim_major_index(i)};
+        int const fdmi{test_array.to_first_dim_major_index(i)};
+        int const fdm_from_ldmi{test_array.to_first_dim_major_index(ldmi)};
+        int const ldm_from_fdmi{test_array.to_last_dim_major_index(fdmi)};
+        if(fdm_from_ldmi != i ) {
+            std::cerr << "Error the Array::to_last_dim_major_index and to_first_dim_major_index are not one another "
+                         "inverse function. Different values were found for i=" << i << " (got ldmi="<< ldmi
+                         << " and fdm_from_ldmi=" << fdm_from_ldmi << ")." << std::endl;
+        }
+        if(ldm_from_fdmi != i ) {
+            std::cerr << "Error the Array::to_last_dim_major_index and to_first_dim_major_index are not one another "
+                         "inverse function. Different values were found for i=" << i << " (got fdmi="<< fdmi
+                      << " and ldm_from_fdmi=" << ldm_from_fdmi << ")." << std::endl;
+        }
+        assert(fdm_from_ldmi == i);
+    }
+
     Index index{dimensions};
     Array_iterator array_index{dimensions};
     {
