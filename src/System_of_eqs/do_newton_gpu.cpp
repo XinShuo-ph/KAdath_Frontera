bbd8ac12c174c078ede9d8a0c53fb1a97a9166ad
Sylvain AULIAC <Sylvain.Auliac@obspm.fr>
2020-11-09 15:36:59 +0100

    error correction for GPU enabled case (2)

--- src/System_of_eqs/do_newton_gpu.cpp
+++ src/System_of_eqs/do_newton_gpu.cpp
@@ -32,7 +32,7 @@ namespace Kadath {
 
 
     template<>
-    bool System_of_eqs::do_newton<Computational_model::gpu_mpi_parallel>(double prec, double &error)
+    bool System_of_eqs::do_newton<Computational_model::gpu_mpi_parallel>(double precision, double &error)
     {
         auto & os = *output_stream;
         bool res;
@@ -46,32 +46,23 @@ namespace Kadath {
         MPI_Comm_rank (MPI_COMM_WORLD, &rank);
         MPI_Comm_size (MPI_COMM_WORLD, &nproc);
 
-        if(rank==0 && niter==1 && verbosity)
-        {
-            display_do_newton_report_header(os,precision);
-        }
         //vars_to_terms();
         Array<double> second (sec_member());
         error = max(fabs(second));
 
         if (error<precision) {
             res = true;
-            if(rank==0 && verbosity)
-            {
-                display_do_newton_ending_line(os,precision,error);
-                os  << endl;
-            }
         }
         else {
-            int nn = second.get_size(0);
-            if (nn!=nbr_unknowns) {
+            int second_member_size = second.get_size(0);
+            if (second_member_size != nbr_unknowns) {
                 cerr << "Number of unknowns is different from the number of equations" << endl;
-                cerr << "nbr equations = " << nn << endl;
+                cerr << "nbr equations = " << second_member_size << endl;
                 cerr << "nbr unknowns  = " << nbr_unknowns << endl;
                 abort();
             }
-            int const nb_cols_per_proc {nn / nproc};
-            int const remaining_cols {nn % nproc};
+            int const nb_cols_per_proc {second_member_size / nproc};
+            int const remaining_cols {second_member_size % nproc};
             int const local_nb_cols {rank < remaining_cols ? nb_cols_per_proc + 1 : nb_cols_per_proc};
             int const local_col_start_idx {rank * nb_cols_per_proc + (rank < remaining_cols ? rank : remaining_cols)};
 //	    if(rank==0)
@@ -83,57 +74,54 @@ namespace Kadath {
 //            }
             
             Hash_key chrono_key = this->start_chrono("MPI parallel do_newton | problem size = ",
-                                                     nn," | matrix computation ");
-            Array<double> local_matrix_slice (nn,local_nb_cols);
+                                                     second_member_size, " | matrix computation ");
+            Array<double> local_matrix_slice (second_member_size, local_nb_cols);
             for(int j{0};j<local_nb_cols;j++)
             {
                 int const jj {j+local_col_start_idx};
                 Array<double> const colj {do_col_J(jj)};
-                for(int i{0};i<nn;i++) local_matrix_slice.set(i, j) = colj(i);
+                for(int i{0}; i < second_member_size; i++) local_matrix_slice.set(i, j) = colj(i);
             }
 	
     	    std::unique_ptr<Array<double>> full_matrix{nullptr};
-	        if(rank==0) full_matrix.reset(new Array<double>(nn,nn));
+	        if(rank==0) full_matrix.reset(new Array<double>(second_member_size, second_member_size));
 
-            std::vector<int> recvcount(nproc,nb_cols_per_proc*nn),displs(nproc,0);
-            assert(recvcount.at(rank) == nn*local_nb_cols);
-            for(auto i = recvcount.begin();i!=recvcount.begin()+remaining_cols;i++) (*i) += nn;
+            std::vector<int> recvcount(nproc, nb_cols_per_proc * second_member_size),displs(nproc, 0);
+            assert(recvcount.at(rank) == second_member_size * local_nb_cols);
+            for(auto i = recvcount.begin();i!=recvcount.begin()+remaining_cols;i++) (*i) += second_member_size;
             for(int i=1;i<nproc;i++) displs.at(i) = displs.at(i-1) + recvcount.at(i-1);
             MPI_Barrier(MPI_COMM_WORLD);
-            MPI_Gatherv(local_matrix_slice.get_data(), nn * local_nb_cols, MPI_DOUBLE, (rank==0 ? full_matrix->set_data() : nullptr),
-                recvcount.data(),displs.data(),MPI_DOUBLE,0,MPI_COMM_WORLD);
+            MPI_Gatherv(local_matrix_slice.get_data(), second_member_size * local_nb_cols, MPI_DOUBLE, (rank == 0 ? full_matrix->set_data() : nullptr),
+                        recvcount.data(), displs.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
             
             Duration const t_load_matrix {this->stop_chrono(chrono_key)};
 
-            Array<double> xx(nn);
+            Array<double> xx(second_member_size);
             Duration  t_trans_matrix,t_inv_matrix;
             if(rank==0) {
                 chrono_key = this->start_chrono("MPI parallel do_newton | problem size = ",
-                                                nn, " | matrix translation ");
-                Magma_matrix magma_mat{*full_matrix,nn};
+                                                second_member_size, " | matrix translation ");
+                Magma_matrix magma_mat{*full_matrix, second_member_size};
         		full_matrix.reset(nullptr);
                 // Translate the second member :
                 Magma_array second_member{second};
                 t_trans_matrix = this->stop_chrono(chrono_key);
 
                 chrono_key = this->start_chrono("MPI parallel do_newton | problem size = ",
-                                                nn, " | matrix inversion ");
+                                                second_member_size, " | matrix inversion ");
                 magma_mat.solve(second_member);
                 t_inv_matrix = this->stop_chrono(chrono_key);
-                for (int i = 0; i < nn; i++) xx.set(i) = second_member[i];
+                for (int i = 0; i < second_member_size; i++) xx.set(i) = second_member[i];
             }
 
-            chrono_key = this->start_chrono("MPI parallel do newton | problem size = ", nn, " | update ");
-            MPI_Bcast(xx.set_data(),nn,MPI_DOUBLE,0,MPI_COMM_WORLD);
+            chrono_key = this->start_chrono("MPI parallel do newton | problem size = ", second_member_size, " | update ");
+            MPI_Bcast(xx.set_data(), second_member_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
             newton_update_vars(xx);
 
             Duration const t_newton_update{this->stop_chrono(chrono_key)};
-
-            if (rank == 0 && verbosity) {
-                display_do_newton_iteration(os,
-                                            {niter,nn,error,t_load_matrix,t_trans_matrix,t_inv_matrix,t_newton_update});
-            }
+            current_output_data = Output_data{niter, second_member_size, error, t_load_matrix, t_trans_matrix,
+                                               t_inv_matrix, t_newton_update};
             res = false;
         }
 #endif //#ifdef ENABLE_GPU_USE
