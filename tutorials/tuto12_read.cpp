a92a2d8ae45f67a1a17bde7c22c1b5c3e7a1d99f
Grandclement Philippe <philippe.grandclement@obspm.fr>
2021-04-27 09:33:12 +0200

    add tuto12

---
+++ tutorials/tuto12_read.cpp
@@ -0,0 +1,34 @@
+#include "kadath.hpp"
+
+using namespace Kadath ;
+int main() {
+
+	// Opening the file in read mode.
+	FILE* fin = fopen ("file.dat", "r") ;
+
+	// Two values to be read
+	int valint  ;
+	double valdouble  ;
+
+	// Read using fread_be
+	fread_be (&valint, sizeof(int), 1, fin) ;
+	fread_be (&valdouble, sizeof(double), 1, fin) ;
+
+	cout << "Integer read " << valint << endl ;
+	cout << "Double read " << valdouble << endl ;
+
+	// Reading Kadath classes
+	// Space first, its exact type must be known
+	Space_spheric space (fin) ;
+	cout << space << endl ;
+
+	// Then the fields
+	Scalar field (space, fin) ;
+	Vector vecU (space, fin) ;
+
+	// Dont forget to close the file in the end
+	fclose(fin) ;
+
+	return EXIT_SUCCESS ;
+}
+
